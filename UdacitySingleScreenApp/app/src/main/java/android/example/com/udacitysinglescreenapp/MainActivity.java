774f39ac7e21e9cc61c7caf1625280967ceae499
Prajkti K <prajktik@gmail.com>
2017-04-28 16:39:34 -0400

    Submitting the Udacity Single Screen App project

---
+++ UdacitySingleScreenApp/app/src/main/java/android/example/com/udacitysinglescreenapp/MainActivity.java
@@ -0,0 +1,21 @@
+package android.example.com.udacitysinglescreenapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
